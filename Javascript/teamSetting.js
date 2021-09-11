7ebbd249dc1261887346e23a8699294a7193463f
Adil Asif <adilasif680@gmail.com>
2021-09-12 01:14:54 +0500

    Delete teamSetting.js

--- Javascript/teamSetting.js
+++
@@ -1,8 +0,0 @@
-function CreateQuestions(question1,question2,question3) {
-    this.question1=question1;
-    this.question2=question2;
-    this.question3=question3;
-}
-
-let questions= new CreateQuestions(" What did you today?", "What will you do tomorrow?","Are there any blockers in your way?");
-localStorage.setItem("Questions",JSON.stringify(questions));
